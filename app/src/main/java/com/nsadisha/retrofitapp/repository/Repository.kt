17ed8546b1ef75df94f067090bd7869e947f19bc
Sadisha Nimsara <nsadisha@gmail.com>
2022-12-02 01:59:40 +0530

    check internet before fetching from the url

--- app/src/main/java/com/nsadisha/retrofitapp/repository/Repository.kt
+++ app/src/main/java/com/nsadisha/retrofitapp/repository/Repository.kt
@@ -1,18 +1,33 @@
 package com.nsadisha.retrofitapp.repository
 
 import com.nsadisha.retrofitapp.api.DevBytesService
+import com.nsadisha.retrofitapp.connection.InternetStatus
 import com.nsadisha.retrofitapp.model.DevByteVideo
-import retrofit2.Response
+import com.nsadisha.retrofitapp.room.VideoDatabase
+import com.nsadisha.retrofitapp.util.Utility.Companion.p
 import javax.inject.Inject
 
 class Repository @Inject constructor(
-    private val devBytesService: DevBytesService
+    private val devBytesService: DevBytesService,
+    private val videoDatabase: VideoDatabase,
+    private val internetStatus: InternetStatus
 )  {
     suspend fun getVideos(): List<DevByteVideo> {
-        val result = devBytesService.getPlayList()
-        if (result.isSuccessful){
-            return result.body()?.asDomainModel()!!
+        try {
+            val isInternetAvailable = internetStatus.isInternetAvailable()
+            if(isInternetAvailable){
+                p("internet")
+                val result = devBytesService.getPlayList()
+                if (result.isSuccessful) {
+                    val videos = result.body()?.asDomainModel()!!
+                    videoDatabase.videoDao.insertAllFromDomainModel(videos)
+                    return videos
+                }
+            }
+            p("no internet")
+            return videoDatabase.videoDao.getVideosAsDomainModel()
+        }catch (e: Exception) {
+            throw Exception("Something went wrong!")
         }
-        return emptyList()
     }
 }
