3bfdacd6fd6bbdb66507aba1b8a16e01977f4720
Sadisha Nimsara <nsadisha@gmail.com>
2022-12-01 12:58:45 +0530

    video model for the application

---
+++ app/src/main/java/com/nsadisha/retrofitapp/model/DevByteVideo.kt
@@ -0,0 +1,9 @@
+package com.nsadisha.retrofitapp.model
+
+data class DevByteVideo(
+    val title: String,
+    val description: String,
+    val url: String,
+    val updated: String,
+    val thumbnail: String
+)
