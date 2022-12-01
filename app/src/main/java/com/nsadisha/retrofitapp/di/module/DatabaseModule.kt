c31c401939cd2cba9ac1456fc41e6ca2fce6120b
Sadisha Nimsara <nsadisha@gmail.com>
2022-12-01 14:14:17 +0530

    database module added

--- app/src/main/java/com/nsadisha/retrofitapp/di/module/DatabaseModule.kt
+++ app/src/main/java/com/nsadisha/retrofitapp/di/module/DatabaseModule.kt
@@ -1,10 +1,24 @@
 package com.nsadisha.retrofitapp.di.module
 
+import android.app.Application
+import android.app.SharedElementCallback
+import androidx.room.Room
+import com.nsadisha.retrofitapp.room.VideoDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(application: Application, callback: VideoDatabase.Callback): VideoDatabase{
+        return Room.databaseBuilder(application, VideoDatabase::class.java, "video_database")
+            .fallbackToDestructiveMigration()
+            .addCallback(callback)
+            .build()
+    }
 }
