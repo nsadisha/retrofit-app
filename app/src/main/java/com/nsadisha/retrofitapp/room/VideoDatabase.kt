2c9cb1595a6cf8a7a5095fcf5082009de2243270
Sadisha Nimsara <nsadisha@gmail.com>
2022-12-01 14:14:43 +0530

    added a callback

--- app/src/main/java/com/nsadisha/retrofitapp/room/VideoDatabase.kt
+++ app/src/main/java/com/nsadisha/retrofitapp/room/VideoDatabase.kt
@@ -1,9 +1,20 @@
 package com.nsadisha.retrofitapp.room
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.nsadisha.retrofitapp.di.module.ApplicationScope
 import com.nsadisha.retrofitapp.room.model.DatabaseVideo
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineScope
+import javax.inject.Inject
+import javax.inject.Provider
 
 @Database(entities = [DatabaseVideo::class], version = 1)
-abstract class VideoDatabase {
+abstract class VideoDatabase: RoomDatabase() {
     abstract val videoDao: VideoDao
+
+    class Callback @Inject constructor(
+        private val database: Provider<VideoDatabase>,
+        @ApplicationScope private val applicationScope: CoroutineScope
+    ): RoomDatabase.Callback()
 }
