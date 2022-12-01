dbbd02381293a50db9ed0595d1c8d4b53b4b6fe2
Sadisha Nimsara <nsadisha@gmail.com>
2022-12-02 02:00:39 +0530

    suspended function with domain model

--- app/src/main/java/com/nsadisha/retrofitapp/room/VideoDao.kt
+++ app/src/main/java/com/nsadisha/retrofitapp/room/VideoDao.kt
@@ -4,13 +4,40 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.nsadisha.retrofitapp.model.DevByteVideo
 import com.nsadisha.retrofitapp.room.model.DatabaseVideo
 
 @Dao
 interface VideoDao {
     @Query("select * from videos")
-    fun getVideos(): List<DatabaseVideo>
+    suspend fun getVideos(): List<DatabaseVideo>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAll(videos: List<DatabaseVideo>)
+    suspend fun insertAll(videos: List<DatabaseVideo>)
+
+    suspend fun getVideosAsDomainModel(): List<DevByteVideo>{
+        return getVideos().map {
+            DevByteVideo(
+                title = it.title,
+                description = it.description,
+                url = it.url,
+                thumbnail = it.thumbnail,
+                updated = it.updated
+            )
+        }
+    }
+
+    suspend fun insertAllFromDomainModel(videos: List<DevByteVideo>){
+        val databaseVideos = videos.map {
+            DatabaseVideo(
+                title = it.title,
+                description = it.description,
+                url = it.url,
+                thumbnail = it.thumbnail,
+                updated = it.updated
+            )
+        }
+
+        insertAll(databaseVideos)
+    }
 }
