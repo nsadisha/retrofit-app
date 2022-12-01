e14726579575e801b7b8be3c66d54b4a13382fec
Sadisha Nimsara <nsadisha@gmail.com>
2022-12-01 13:16:51 +0530

    asDomainModel function added

--- app/src/main/java/com/nsadisha/retrofitapp/api/model/NetworkVideoContainer.kt
+++ app/src/main/java/com/nsadisha/retrofitapp/api/model/NetworkVideoContainer.kt
@@ -1,5 +1,19 @@
 package com.nsadisha.retrofitapp.api.model
 
+import com.nsadisha.retrofitapp.model.DevByteVideo
+
 data class NetworkVideoContainer(
     val videos: List<NetworkVideo>
-)
+){
+    fun asDomainModel(): List<DevByteVideo>{
+        return videos.map {
+            DevByteVideo(
+                title = it.title,
+                description = it.description,
+                url = it.url,
+                updated = it.updated,
+                thumbnail = it.thumbnail
+            )
+        }
+    }
+}
