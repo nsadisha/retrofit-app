cd4fade84f9265dfc0621db120bab72d839406c7
Sadisha Nimsara <nsadisha@gmail.com>
2022-12-01 13:17:59 +0530

    getVideos() function returns a list of videos instead of NetworkVideoContainer

--- app/src/main/java/com/nsadisha/retrofitapp/repository/Repository.kt
+++ app/src/main/java/com/nsadisha/retrofitapp/repository/Repository.kt
@@ -1,14 +1,18 @@
 package com.nsadisha.retrofitapp.repository
 
 import com.nsadisha.retrofitapp.api.DevBytesService
-import com.nsadisha.retrofitapp.api.model.NetworkVideoContainer
+import com.nsadisha.retrofitapp.model.DevByteVideo
 import retrofit2.Response
 import javax.inject.Inject
 
 class Repository @Inject constructor(
     private val devBytesService: DevBytesService
 )  {
-    suspend fun getVideos(): Response<NetworkVideoContainer> {
-        return devBytesService.getPlayList()
+    suspend fun getVideos(): List<DevByteVideo> {
+        val result = devBytesService.getPlayList()
+        if (result.isSuccessful){
+            return result.body()?.asDomainModel()!!
+        }
+        return emptyList()
     }
 }
